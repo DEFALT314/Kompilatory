2c4703977c556870f5914d3c9af52bcfe65d6850
DEFALT314 <klwid04@gmail.com>
2025-10-26 13:15:15 +0100

    nie dziala

--- LAB2/parser.py
+++ LAB2/parser.py
@@ -10,9 +10,15 @@ class Mparser(Parser):
 
 
     precedence = (
-    # to fill ...
-        ("left", '+', '-'),
-    # to fill ...
+    ("nonassoc", 'IFX'),
+    ("nonassoc", 'ELSE'),
+    ("left", 'ADD', 'SUB'),
+    ("left", 'MUL', 'DIVIDE'),
+    ("right", '=', 'MULASSIGN', 'DIVASSIGN', 'ADDASSIGN', 'SUBASSIGN'),
+    ("nonassoc", 'EQ', 'LT', 'LE', 'GT', 'GE', 'NE'),
+    ("left", 'DOTADD', 'DOTSUB'),
+    ("left", 'DOTMUL', 'DOTDIV'),
+    # ("right", 'UMINUS')
     )
 
 
@@ -37,6 +43,107 @@ class Mparser(Parser):
         pass
 
 
-    # to finish the grammar
-    # ....
+    @_('assignment',
+       'if_stmt',
+       'while_stmt',
+       'for_stmt',
+       'flow_stmt',
+       'print_stmt',
+       'block_stmt',
+       'expr ";"')
+    def instruction(p):
+        pass
+    
+    @_('BREAK ";"',
+       'CONTINUE ";"',
+       'RETURN ";"',
+       'RETURN expr ";"')
+    def flow_stmt(p):
+        pass
 
+    @_('IF "(" expr ")" instruction ELSE instruction',
+       'IF "(" expr ")" instruction %prec IFX')
+    def if_stmt(p):
+        pass
+    
+    @_('WHILE "(" expr ")" instruction')
+    def while_stmt(p):
+        pass
+    
+    @_('FOR ID "=" expr ":" expr instruction')
+    def for_stmt(p):
+        pass
+    
+    @_('"{" instructions "}"')
+    def block_stmt(p):
+        pass
+    
+    @_('PRINT expr ";"')
+    def print_stmt(p):
+        pass
+    
+    @_('lvalue ASSIGN expr',
+       'lvalue ADDASSIGN expr',
+       'lvalue SUBASSIGN expr',
+       'lvalue MULASSIGN expr',
+       'lvalue DIVASSIGN expr')
+    def assignment(p):
+        pass
+    
+    @_('ID',
+       'ID "[" expr_list "]"')
+    def lvalue(p):
+        pass
+
+    @_('expr ADD expr',
+       'expr SUB expr',
+       'expr MUL expr',
+       'expr DIVIDE expr',
+       'expr DOTADD expr',
+       'expr DOTSUB expr',
+       'expr DOTMUL expr',
+       'expr DOTDIV expr',
+       'expr EQ expr',
+       'expr NE expr',
+       'expr LT expr',
+       'expr LE expr',
+       'expr GT expr',
+       'expr GE expr')
+    def expr(p):
+        pass
+    
+    
+    @_('expr "\'"',
+       'ID',
+       'INTNUM',
+       'FLOAT',
+       'STRING',
+       'matrix',
+       'EYE "(" expr_list ")"',
+       'ZEROS "(" expr_list ")"',
+       'ONES "(" expr_list ")"',
+       'ID "(" expr_list ")"',
+       'ID "[" expr_list "]"',
+       '"(" expr ")"')
+    def expr(p):
+        pass
+    
+    
+    @_('expr')
+    def expr_list(p):
+        pass
+
+    @_('expr_list "," expr')
+    def expr_list(p):
+        pass
+    @_(' "[" row_list "]" ')
+    def matrix(p):
+        pass
+    
+    @_('row_list ";" row', 'row')
+    def row_list(p):
+        pass
+    
+    @_('expr_list')
+    def row(p):
+        pass
--- LAB2/scanner.py
+++ LAB2/scanner.py
@@ -15,7 +15,6 @@ class Scanner(Lexer):
         'BREAK', 'CONTINUE', 'RETURN',
         'EYE', 'ZEROS', 'ONES',
         'PRINT',
-        'INT',
         'FLOAT',
         "STRING",
         }
